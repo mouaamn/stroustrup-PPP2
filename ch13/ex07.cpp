0a6654641b22ba999e9929796699153a1d5c11d2
mouaamn <mouaamnalali@gmail.com>
2026-05-24 16:41:24 +0800

    Chapter 13 ex07

---
+++ ch13/ex07.cpp
@@ -0,0 +1,33 @@
+#include <Simple_window.h>
+#include <Graph.h>
+
+int main()
+
+try {
+	using namespace Graph_lib;
+
+	const Point TL{ 100, 100 };
+	Simple_window win{ TL, 600, 400, "Chapter 13: Exercise 7" };
+
+	Vector_ref<Rectangle> rcs;
+	constexpr int SIZE{ 20 };
+
+	for (int i{}; i < 16; ++i) {
+		for (int j{}; j < 16; ++j) {
+			rcs.push_back(new Rectangle{ { SIZE * i, SIZE * j }, SIZE, SIZE });
+			const int INDEX{ i * 16 + j };
+			rcs[INDEX].set_fill_color(INDEX);
+			win.attach(rcs[INDEX]);
+		}
+	}
+
+	win.wait_for_button();
+}
+catch (exception& e) {
+	cerr << "Exception: " << e.what() << '\n';
+	return 1;
+}
+catch (...) {
+	cerr << "Unknown exception" << '\n';
+	return 1;
+}
