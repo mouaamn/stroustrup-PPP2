19787621adde4cc2e192d69d57f98499ad8092ef
mouaamn <mouaamnalali@gmail.com>
2026-05-26 05:06:15 +0800

    Chapter 14 ex03

--- ch14/ex02-03.cpp
+++ ch14/ex02-03.cpp
@@ -1,4 +1,8 @@
 /*
     Try to copy a Shape. What happens?
     > You can't create an object of Shape to begin with, because it's an abstract class.
+
+    Define an abstract class and try to define an object of that type. What 
+    happens?
+    > You get an error.
 */
